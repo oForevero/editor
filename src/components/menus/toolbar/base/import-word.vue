0627a87fec85f8355c185a50a023fbe386e981a5
likai <2818591096@qq.com>
2025-05-19 17:16:30 +0800

    导入组件调整，打字机方法等预留
    (cherry picked from commit 204fcfaf41128acdfc600f806cfe7ad784eb16f1)

--- src/components/menus/toolbar/base/import-word.vue
+++ src/components/menus/toolbar/base/import-word.vue
@@ -1,141 +1,133 @@
+<!-- import-word.vue -->
 <template>
   <menus-button
     v-if="options.toolbar?.importWord?.enabled"
     ico="word"
     :text="t('base.importWord.text')"
     huge
-    @menu-click="importWord"
+    @menu-click="showDialog = true"
   />
+
+  <emo-dialog
+    :visible="showDialog"
+    :header="t('base.importWord.dialogTitle')"
+    width="560px"
+    @confirm="onConfirm"
+    @close="showDialog = false"
+  >
+    <template #default>
+      <input ref="uploadInput" type="file" accept=".docx" />
+    </template>
+  </emo-dialog>
 </template>
 
 <script setup lang="ts">
+import EmoDialog from '@/components/modal.vue'
+import { ref, inject, onMounted } from 'vue'
+
 const container = inject('container')
 const editor = inject('editor')
 const options = inject('options')
 
-// 动态导入 mammoth.js
+const showDialog = ref(false)
+const uploadInput = ref<HTMLInputElement>()
+
 onMounted(() => {
   const mammothScriptElement = document.querySelector('#mammoth-script')
   if (
     mammothScriptElement === null &&
     options.value.toolbar?.importWord.enabled
   ) {
-    const style = document.createElement('script')
-    style.src = `${options.value.cdnUrl}/libs/mammoth/mammoth.browser.min.js`
-    style.id = 'mammoth-script'
-    document.querySelector('head')?.append(style)
+    const script = document.createElement('script')
+    script.src = `${options.value.cdnUrl}/libs/mammoth/mammoth.browser.min.js`
+    script.id = 'mammoth-script'
+    document.head.appendChild(script)
   }
 })
 
-const importWord = () => {
-  // @ts-expect-error, global variable injected by script
-  if (!mammoth) {
-    const dialog = useAlert({
+const onConfirm = () => {
+  const file = uploadInput.value?.files?.[0]
+  if (!file) return
+  handleWordImport(file);
+  showDialog.value = false;
+}
+
+const handleWordImport = async (file: File) => {
+  if (!file) return
+  if (file.size > 5 * 1024 * 1024) {
+    useMessage('error', {
       attach: container,
-      theme: 'warning',
-      header: t('base.importWord.loadScript.title'),
-      body: t('base.importWord.loadScript.message'),
-      onConfirm() {
-        dialog.destroy()
-      },
+      content: t('base.importWord.limitSize'),
     })
     return
   }
-  const { open, onChange } = useFileDialog({
-    accept: '.docx',
-    reset: true,
-    multiple: false,
-  })
-  // 打开文件对话框
-  open()
-  // 插入文件
-  onChange(async (files: FileList | null) => {
-    const [file] = Array.from(files ?? [])
-    if (!file) {
-      return
-    }
-    if (file.size > 1024 * 1024 * 5) {
-      useMessage('error', {
-        attach: container,
-        content: t('base.importWord.limitSize'),
-      })
-      return
-    }
-    const message = await useMessage('loading', {
-      attach: container,
-      content: t('base.importWord.converting'),
-    })
 
-    // 使用用户自定义导入方法
-    if (options.value.toolbar?.importWord?.useCustomMethod) {
+  const message = await useMessage('loading', {
+    attach: container,
+    content: t('base.importWord.converting'),
+  })
+
+  if (options.value.toolbar?.importWord?.useCustomMethod) {
+    try {
       const result =
         await options.value.toolbar?.importWord.onCustomImportMethod?.(file)
       message.close()
-      try {
-        if (result?.messages?.type === 'error') {
-          useMessage('error', {
-            attach: container,
-            content: `${t('base.importWord.convertError')} (${result.messages.message})`,
-          })
-          return
-        }
-        if (result?.value) {
-          editor.value?.commands.setContent(result.value)
-        } else {
-          useMessage('error', {
-            attach: container,
-            content: t('base.importWord.importError'),
-          })
-        }
-      } catch {
+      if (result?.messages?.type === 'error') {
+        useMessage('error', {
+          attach: container,
+          content: `${t('base.importWord.convertError')} (${result.messages.message})`,
+        })
+        return
+      }
+      if (result?.value) {
+        editor.value?.commands.setContent(result.value)
+      } else {
         useMessage('error', {
           attach: container,
           content: t('base.importWord.importError'),
         })
       }
-      return
-    }
-
-    // 默认使用 Mammoth 导入
-    const arrayBuffer = file.arrayBuffer()
-    // @ts-expect-error, global variable injected by script
-    if (!mammoth) {
-      return
-    }
-    // @ts-expect-error, global variable injected by script
-    const { messages, value } = await mammoth.convertToHtml(
-      { arrayBuffer },
-      options.value.toolbar?.importWord.options,
-    )
-    message.close()
-    if (messages.type === 'error') {
-      useMessage(
-        'error',
-        `${t('base.importWord.convertError')} (${messages.message})`,
-      )
-      return
-    }
-    try {
-      // 解析和加工 Mammoth 返回的 HTML 内容
-      const domparser = new DOMParser()
-      const doc = domparser.parseFromString(value, 'text/html')
-      for (const img of doc.querySelectorAll('img')) {
-        const parent = img.parentElement
-        if (parent?.tagName === 'P') {
-          parent.insertAdjacentElement('beforebegin', img)
-          if (!parent.hasChildNodes() && parent.textContent === '') {
-            parent.remove()
-          }
-        }
-      }
-      const content = doc.body.innerHTML.toString()
-      editor.value?.commands.setContent(content)
     } catch {
       useMessage('error', {
         attach: container,
         content: t('base.importWord.importError'),
       })
     }
-  })
+    return
+  }
+
+  const arrayBuffer = await file.arrayBuffer()
+  // @ts-expect-error
+  const { messages, value } = await mammoth.convertToHtml(
+    { arrayBuffer },
+    options.value.toolbar?.importWord.options,
+  )
+  message.close()
+  if (messages.type === 'error') {
+    useMessage('error', {
+      attach: container,
+      content: `${t('base.importWord.convertError')} (${messages.message})`,
+    })
+    return
+  }
+
+  try {
+    const doc = new DOMParser().parseFromString(value, 'text/html')
+    for (const img of doc.querySelectorAll('img')) {
+      const parent = img.parentElement
+      if (parent?.tagName === 'P') {
+        parent.insertAdjacentElement('beforebegin', img)
+        if (!parent.hasChildNodes() && parent.textContent === '') {
+          parent.remove()
+        }
+      }
+    }
+    editor.value?.commands.setContent(doc.body.innerHTML)
+  } catch {
+    useMessage('error', {
+      attach: container,
+      content: t('base.importWord.importError'),
+    })
+  }
 }
 </script>
